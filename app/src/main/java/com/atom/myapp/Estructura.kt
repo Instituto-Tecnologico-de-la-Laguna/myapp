09d4fb0bfcdf4aa0b475e1599f571fad5562e7b6
jmot4 <jmota@gtech-services.com>
2026-08-28 13:48:19 -0600

    Se agregan funciones y queda pendiente Lazy Column

---
+++ app/src/main/java/com/atom/myapp/Estructura.kt
@@ -0,0 +1,11 @@
+package com.atom.myapp
+
+import android.graphics.Color
+
+data class Estructura(
+    val nombre: String,
+    val color: Color
+)
+
+
+
--- app/src/main/java/com/atom/myapp/MainActivity.kt
+++ app/src/main/java/com/atom/myapp/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.atom.myapp
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.atom.myapp.ui.theme.MyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Formulario()
                 }
             }
         }
@@ -31,17 +44,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Formulario(){
+    LazyColumn(
+        contentPadding = PaddingValues(12.dp)
+    ){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyAppTheme {
-        Greeting("Android")
     }
+}
+@Composable
+fun Espacio(espacio: Int){
+    Spacer(modifier=Modifier.padding(espacio.dp))
+}
+@Composable
+fun Texto(texto:String,color:Color){
+    Text(
+        text=texto,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+        color= color,
+        textAlign = TextAlign.Center,
+        modifier= Modifier
+            .background(Color.Black)
+            .fillMaxWidth()
+    )
 }
