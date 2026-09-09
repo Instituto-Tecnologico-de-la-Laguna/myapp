2544dafb220e26db14674cbb7a7d858f9616a477
jmot4 <jmota@gtech-services.com>
2026-09-09 13:51:04 -0600

    Modificacion de iconos

--- app/src/main/java/com/atom/myapp/MainActivity.kt
+++ app/src/main/java/com/atom/myapp/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.atom.myapp
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.atom.myapp.ui.theme.MyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Screen()
                 }
             }
         }
@@ -31,17 +38,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyAppTheme {
-        Greeting("Android")
+fun Screen(){
+    Row(
+       verticalAlignment = Alignment.CenterVertically
+    ){
+        Icon(
+            imageVector = ,
+            contentDescription = "Prueba",
+            tint=Color.Red,
+            modifier = Modifier.size(17.dp)
+        )
     }
 }
+
