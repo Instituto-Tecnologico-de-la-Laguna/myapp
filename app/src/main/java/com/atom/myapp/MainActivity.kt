95098d5a4aeae351396882eb1afce4a6fd816856
jmot4 <jmota@gtech-services.com>
2026-09-09 23:11:02 -0600

    Version 2 de iconos

--- app/src/main/java/com/atom/myapp/MainActivity.kt
+++ app/src/main/java/com/atom/myapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,12 +18,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.atom.myapp.ui.theme.MyAppTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,15 +44,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Screen(){
-    Row(
-       verticalAlignment = Alignment.CenterVertically
+    Column (
+       modifier= Modifier
+           .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
         Icon(
-            imageVector = ,
+            imageVector =Icons.Filled.Favorite ,
             contentDescription = "Prueba",
             tint=Color.Red,
-            modifier = Modifier.size(17.dp)
+            modifier = Modifier.size(50.dp)
         )
+
     }
 }
 
