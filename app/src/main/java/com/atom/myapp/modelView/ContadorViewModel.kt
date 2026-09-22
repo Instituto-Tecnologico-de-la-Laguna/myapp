79dd158db35af03bf9443bc27e5d66317d794e5c
jmot4 <jmota@gtech-services.com>
2026-09-22 13:49:57 -0600

    Primer paso para navegacion

---
+++ app/src/main/java/com/atom/myapp/modelView/ContadorViewModel.kt
@@ -0,0 +1,14 @@
+package com.atom.myapp.modelView
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class ContadorViewModel: ViewModel(){
+    private val _contador= mutableStateOf(0)
+    val contador: State<Int> = _contador
+
+    fun add(){
+        _contador.value = _contador.value + 1
+    }
+}
---
+++ app/src/main/java/com/atom/myapp/views/ContadorView.kt
@@ -0,0 +1,24 @@
+package com.atom.myapp.views
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.atom.myapp.modelView.ContadorViewModel
+
+
+@Composable
+fun ContadorView(view : ContadorViewModel){
+    Box(
+        contentAlignment= Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(text = view.contador.value.toString(),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold)
+    }
+}
